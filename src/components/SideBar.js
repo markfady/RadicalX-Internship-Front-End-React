a7615ec43bf3032d1530b91111e22754ddcf83d6
Markfady <47190433+markfady@users.noreply.github.com>
2022-10-31 01:21:20 +0200

    Add files via upload

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,7 +3,7 @@ import Logo from "../Images/RadicallX-Logo 1.png"
 function SideBar(){
     return(
         <>
-        <div className="container-fluid pt-5 ps-4">
+        <div className="container-fluid pt-5 ps-4 Sidebar-cont">
             <div className="SideBar ">
             <img className="ml-2 ms-4" src={Logo} alt="Logo"/>
             <div className="SideBar-options">
@@ -16,9 +16,9 @@ function SideBar(){
                     <i className="fa-regular fa-star fa-lg me-2"></i>
                     <li className="mb-3 ps-2">Apperenterships</li>
                     </div>
-                    <div className="options d-flex">
-                    <i className="fa-solid fa-book-open fa-lg me-2"></i>
-                    <li className="mb-3 ps-2">Internships</li>
+                    <div className="options active-sidebar d-flex">
+                    <i className=" pt-2 fa-solid fa-book-open fa-lg me-2"></i>
+                    <li className="mb-3 ps-2 pt-1 active-sidebar">Internships</li>
                     </div>
                     <div className="options d-flex">
                     <i className="fa-solid fa-briefcase fa-lg me-3"></i>
