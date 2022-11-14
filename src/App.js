40ac9414800da4eee6cd89533ccccaf8a75b7447
Markfady <47190433+markfady@users.noreply.github.com>
2022-11-14 23:50:31 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 //import './App.css';
 
 import InternshipInsights from "./components/InternshipInsights";
-//import {BrowserRouter , Route, Routes} from "react-router-dom";
+import CreateNewInternship from "./components/CreateNewInternship"
+import Showhidetab from "./components/Buttons"
+import {BrowserRouter as Router , Route, Routes} from "react-router-dom";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <div className="App">
-    <InternshipInsights/>   {/* Week 1-2 Task*/}
-    </div>
+    <> 
+      <Router>
+        <Routes> 
+          <Route path="/" element={<InternshipInsights />} />  {/* Week 1-2 Task*/}
+          <Route path="/CreateNewInternship" element={<CreateNewInternship/>} />  {/*Week 3-4 Task */}
+          <Route path="/CreateNewInternship" element={<SearchBar/>} />
+          <Route path="/Showhidetab" element={<Showhidetab/>} />
+        </Routes>
+      </Router>
+      </>
+
   );
 }
 
